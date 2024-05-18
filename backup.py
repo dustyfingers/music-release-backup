a25a22ab9bf0c56124700d671fddfa55107d99ef
lou <dustyfingers01257@gmail.com>
2024-05-18 08:59:53 -0600

    work on fixing oauth creds

---
+++ backup.py
@@ -0,0 +1,55 @@
+import os.path
+from zipfile import ZipFile
+
+import google.auth
+from googleapiclient.discovery import build
+from googleapiclient.errors import HttpError
+from googleapiclient.http import MediaFileUpload
+
+# If modifying these scopes, delete the file token.json.
+SCOPES = ["https://www.googleapis.com/auth/drive.metadata.readonly"]
+
+# need to use absolute filepath for this to work when called from the cmd
+BACKUP_FILEPATH = r"C:\Release Backups"
+TOKEN_FILEPATH = r".\token.json"
+
+def backup():
+    """Insert new file.
+    Returns : Id's of the file uploaded
+
+    Load pre-authorized user credentials from the environment.
+    TODO(developer) - See https://developers.google.com/identity
+    for guides on implementing OAuth2 for the application.
+    """
+    creds, _ = google.auth.default()
+
+    print(creds)
+
+    # for each zip file in release backups, upload it to my google drive
+    for file_name in os.listdir(BACKUP_FILEPATH):
+        try:
+            with ZipFile(file_name, "r") as zip:
+                zip.extractall()
+                zip.close()
+        except ZipFile.BadZipError:
+            print("bad zip file!")
+
+    try:
+        # create drive api client
+        service = build("drive", "v3", credentials=creds)
+
+        file_metadata = {"name": "download.jpeg"}
+        media = MediaFileUpload("download.jpeg", mimetype="image/jpeg")
+        # pylint: disable=maybe-no-member
+        file = (
+            service.files()
+            .create(body=file_metadata, media_body=media, fields="id")
+            .execute()
+        )
+        print(f'File ID: {file.get("id")}')
+
+    except HttpError as error:
+        print(f"An error occurred: {error}")
+        file = None
+
+    return file.get("id")
--- readme.md
+++ readme.md
@@ -1,8 +1,4 @@
-will try in python first then switch to js if I can't get it working quick enough.
-
-this is made to be scheduled as a task with the windows task scheduler :) so you can make the program that is started 'python' or the absolute path to your python installation, and then pass this file's absolute path as the only argument
-
-you will need a token.json file for the google cloud integration in the root directory of this project.
+this is made to be scheduled as a task with the windows task scheduler
 
 to run this project you will need:
 
@@ -10,3 +6,7 @@ to run this project you will need:
 2. a token.json with the proper creds from google for oauth
 3. a scheduled task set up on your windows computer that dictates that the program that is started is 'python' or the absolute path to your python installation, and then this file's absolute path as the only argument
 4. files to back up to google cloud. I hardcoded my personal release backup folder's absolute path but this should be able to work with any directory. check the BACKUP_FILEPATH constant in main.py
+
+formatting:
+
+I decided to use black formatter, so run `pip install black` and then `black file_or_folder_name` to format :)
